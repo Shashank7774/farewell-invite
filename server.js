21815881a7a652855c8450a32f4132090561e265
Shashank vaish <shashankvaish72@gmail.com>
2026-04-29 22:58:04 +0530

    fixed structure final

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
-app.use(express.static(__dirname)); // 👈 IMPORTANT
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/card", (req, res) => {
-  res.sendFile(path.join(__dirname, "invcard.html"));
+  res.sendFile(path.join(__dirname, "public/invcard.html"));
 });
 
 app.listen(PORT, () => {
